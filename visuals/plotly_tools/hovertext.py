36ea7c5ab8d9c29e07af0266ec2ea3310d576bf0
Nick Weppenaar <weppenaar@gmail.com>
2021-04-04 20:15:30 +0200

    Rounding of temperature.

--- visuals/plotly_tools/hovertext.py
+++ visuals/plotly_tools/hovertext.py
@@ -3,7 +3,7 @@ import pandas as pd
 def braz_cities_temp(series: pd.Series, city_name: str, timespan) -> list:
     text_list = [
         f"{city_name} {idx.title()} {timespan}<br>" +
-        f"{item} Deg C"
+        f"{round(item, 1)} Deg C"
         for idx, item in series.iteritems()
     ]
 
