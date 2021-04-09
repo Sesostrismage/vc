a59da511058c021661ecb32918016772e0a063a0
Nick Weppenaar <weppenaar@gmail.com>
2021-04-09 13:10:13 +0200

    Load function for all cities together over time.

--- data_io/files.py
+++ data_io/files.py
@@ -1,5 +1,9 @@
+import datetime
 import numpy as np
+import os
 import pandas as pd
+
+from vc.data_io import files
 
 def braz_cities_temp(file_path: str) -> pd.DataFrame:
     # Load data into Pandas DataFrame with first row as column names and first column as index names.
@@ -15,3 +19,23 @@ def braz_cities_temp(file_path: str) -> pd.DataFrame:
     df_crop[df_crop > 100] = np.nan
 
     return df_crop
+
+
+def braz_cities_temp_all(folder_path: str) -> pd.DataFrame:
+    # Get file name list from the folder.
+    file_name_list = os.listdir(folder_path)
+
+    df = pd.DataFrame()
+
+    for file_name in file_name_list:
+        # Load data into Pandas DataFrame with first row as column names and first column as index names.
+        city_df = files.braz_cities_temp(os.path.join(folder_path, file_name))
+        city_df.columns = [idx+1 for idx, _ in enumerate(city_df.columns)]
+        stacked_df = city_df.stack()
+        stacked_df.index = [datetime.date(i[0], i[1], 1) for i in stacked_df.index]
+        df = pd.concat([df, pd.DataFrame({file_name: stacked_df})], axis=1)
+
+    df.sort_index(inplace=True)
+    df.columns = [file_name[8:-4].replace('_', ' ').title() for file_name in df.columns]
+
+    return df
--- datasets/temp_brazil_cities/07_other_ways_of_plotting.py
+++ datasets/temp_brazil_cities/07_other_ways_of_plotting.py
@@ -14,28 +14,6 @@ stt.settings()
 # Folder path to data files.
 folder_path = r"C:/Data/temperature_time-series_for_brazilian_cities/"
 
+df = files.braz_cities_temp_all(folder_path)
 
-# Get file name list from the folder.
-file_name_list = os.listdir(folder_path)
-
-df = pd.DataFrame()
-
-for file_name in file_name_list:
-    # Load data into Pandas DataFrame with first row as column names and first column as index names.
-    city_df = files.braz_cities_temp(os.path.join(folder_path, file_name))
-    city_df.columns = [idx+1 for idx, _ in enumerate(city_df.columns)]
-
-    city_series = pd.Series(name=file_name)
-
-    for idx, row in city_df.iterrows():
-        for month in city_df.columns:
-            city_series = city_series.append(
-                pd.Series(
-                    [row[month]],
-                    index=[datetime.date(idx, month, 1)])
-            )
-
-    df = pd.concat([df, pd.DataFrame({file_name: city_series})], axis=1)
-
-df.sort_index(inplace=True)
 st.dataframe(df)
