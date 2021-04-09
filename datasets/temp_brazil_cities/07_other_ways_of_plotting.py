116d0707a45912e638cfbf1781c4fd92acc19424
Nick Weppenaar <weppenaar@gmail.com>
2021-04-09 13:27:05 +0200

    Date sliders.

--- datasets/temp_brazil_cities/07_other_ways_of_plotting.py
+++ datasets/temp_brazil_cities/07_other_ways_of_plotting.py
@@ -16,4 +16,36 @@ folder_path = r"C:/Data/temperature_time-series_for_brazilian_cities/"
 
 df = files.braz_cities_temp_all(folder_path)
 
-st.dataframe(df)
+city = st.sidebar.selectbox(
+    'Choose city',
+    options=df.columns
+)
+
+date_start = st.sidebar.select_slider(
+    'Start date',
+    options=list(df.index)
+)
+
+date_end = st.sidebar.select_slider(
+    'End date',
+    options=list(df.index)
+)
+
+if date_start >= date_end:
+    st.error('Date start is greater than date end!')
+    st.stop()
+
+range_df = df.loc[date_start:date_end]
+
+city_series = range_df[city]
+
+fig = go.Figure()
+
+# Plot data from selected year with hovertext.
+fig.add_trace(
+    go.Scattergl(
+        x=city_series.index,
+        y=city_series
+    )
+)
+st.plotly_chart(fig)
