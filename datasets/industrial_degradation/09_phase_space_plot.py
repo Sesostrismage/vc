e55461e6846de8191e83b298b9477a8f833e2826
Nick Weppenaar <weppenaar@gmail.com>
2021-04-11 20:55:32 +0200

    First plot of mining data.

--- datasets/industrial_degradation/09_phase_space_plot.py
+++ datasets/industrial_degradation/09_phase_space_plot.py
@@ -1,65 +1,81 @@
+import datetime
 import os
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
 
+import vc.visuals.plotly_tools.layout as pt_layout
 import vc.visuals.streamlit_tools as stt
 
 stt.settings()
-data_folder = r"C:/Data/one_year_industrial_component_degradation"
-df = pd.read_pickle(os.path.join(data_folder, 'agg_data.pkl'))
-df.columns = ['timestamp', 'pCut motor torque', 'pCut lag error', 'pCut actual position', 'pCut actual speed', 'pFilm actual position', 'pFilm actual speed', 'pFilm lag error', 'pSpintor VAX speed', 'Mode']
+data_folder = r"C:/Data/mining"
+df = pd.read_csv(
+    os.path.join(data_folder, 'MiningProcess_Flotation_Plant_Database.csv'),
+    parse_dates=['date'],
+    decimal=',',
+    index_col=0,
+    header=0
+)
+df.index = pd.to_datetime(df.index)
 st.dataframe(df.head())
 
 st.info(f"Total data size: {len(df)} rows.")
 
-t_start = st.sidebar.number_input(
-    'Choose start datetime',
-    min_value=df.index[0],
-    max_value=df.index[-1]
+date_start = st.sidebar.date_input(
+    'Data start date',
+    min_value=df.index[0].date(),
+    max_value=df.index[-1].date(),
+    value=df.index[0].date()
 )
 
-timespan = st.sidebar.number_input(
-    'Choose number of seconds to show',
-    min_value=1.,
-    max_value=df.index[-1] - t_start,
-    value=100.
+date_end = st.sidebar.date_input(
+    'Data start date',
+    min_value=df.index[0].date(),
+    max_value=df.index[-1].date(),
+    value=df.index[-1].date()
 )
+
+plot_df = df[(df.index >= datetime.datetime.combine(date_start, datetime.datetime.min.time())) & (df.index <= datetime.datetime.combine(date_end, datetime.datetime.min.time()))]
+
+options = list(df.columns)
 
 x = st.sidebar.selectbox(
-    'Choose x variable',
-    options=df.columns,
-    index=1
+    'Choose X variable',
+    options=['Datetime'] + options,
+    index=0
 )
+if x == 'Datetime':
+    x = plot_df.index
+else:
+    x = plot_df[x]
 
 y = st.sidebar.selectbox(
     'Choose y variable',
-    options=df.columns,
-    index=2
+    options=options,
+    index=1
 )
 
 color = st.sidebar.selectbox(
     'Choose color variable',
-    options=df.columns,
-    index=len(df.columns)-1
+    options=options,
+    index=2
 )
-
-plot_df = df[(df.index >= t_start) & (df.index <= t_start + timespan)]
 
 fig = go.Figure()
 
-
-
-fig.update_layout(height=800, width=1100, hovermode='closest', showlegend=False)
-
 fig.add_trace(
     go.Scattergl(
-        x=plot_df[x],
+        x=x,
         y=plot_df[y],
         mode='markers',
         marker={'color': plot_df[color]}
     )
 )
+
+fig.update_layout(
+        height=pt_layout.height_standard,
+        width=pt_layout.width_standard
+    )
 
 st.plotly_chart(fig)
