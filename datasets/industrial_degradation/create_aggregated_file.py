47ed611c0319f75157cd6b9432cab6055318fa6e
Nick Weppenaar <weppenaar@gmail.com>
2021-04-11 20:26:58 +0200

    Saving blabla.

--- datasets/industrial_degradation/create_aggregated_file.py
+++ datasets/industrial_degradation/create_aggregated_file.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import pandas as pd
 
@@ -6,25 +7,32 @@ raw_data_folder = os.path.join(folder, 'raw_data')
 
 raw_file_list = os.listdir(raw_data_folder)
 
+start_dt = datetime.datetime(2020, 1, 1)
 
-for file_name in os.listdir(raw_data_folder):
-    print(file_name)
-    mode = int(file_name[-5])
+for fn in os.listdir(raw_data_folder):
+    print(fn)
+    month = int(fn[0:2])
+    day = int(fn[3:5])
+    hour = int(fn[6:8])
+    minute = int(fn[8:10])
+    second = int(fn[10:12])
+    delta = datetime.timedelta(days=30*month + day, hours=hour, minutes=minute, seconds=second)
+    print(delta)
+    mode = int(fn[-5])
 
-    if file_name == raw_file_list[0]:
+    if fn == raw_file_list[0]:
         df = pd.read_csv(
-            os.path.join(raw_data_folder, file_name),
-            index_col=0,
+            os.path.join(raw_data_folder, fn),
             header=0
         )
         df['mode'] = mode
+        df.index = start_dt + delta + pd.to_timedelta(df['timestamp'], unit='s')
     else:
         file_df = pd.read_csv(
-            os.path.join(raw_data_folder, file_name),
-            index_col=0,
+            os.path.join(raw_data_folder, fn),
             header=0
         )
-        file_df.index = file_df.index + df.index[-1]
+        file_df.index = start_dt + delta + pd.to_timedelta(file_df['timestamp'], unit='s')
         file_df['mode'] = mode
 
         df = pd.concat([df, file_df])
