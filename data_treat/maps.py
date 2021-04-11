da7652dfa7853cea4137540d52cb702f2058a495
Nick Weppenaar <weppenaar@gmail.com>
2021-04-11 09:53:37 +0200

    Initial commit.

---
+++ data_treat/maps.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+def map_range(
+    in_series: pd.Series,
+    in_min: float=None,
+    in_max: float=None,
+    out_min: float=0.,
+    out_max: float=1.,
+    invert: bool=False,
+    to_int: bool=False
+) -> pd.Series:
+    """
+    This function maps a data range to a new data range.
+
+    Args:
+        in_series (pd.Series): Series with range to be mapped.
+        in_min (float, optional): Minimum for the original series to use in the
+            mapping. if None, takes the minimum of the series.
+            If greater than the minimum of the series, the series
+            will be mapped with clipping.
+        in_max (float, optional): Maximum for the original series to use in the
+            mapping. if None, takes the maximum of the series.
+            If less than the maximum of the series, the series
+            will be mapped with clipping.
+        out_min (float, optional): Minimum of the mapped series. Defaults to 0..
+        out_max (float, optional): Maximum of the mapped series. Defaults to 1..
+        invert (bool, optional): Whether or not the mapped
+            series should be inverted with regards to the original series.
+            Defaults to False.
+        to_int (bool, optional): Whether or not the mapped
+            series should be should be cast to int for use in e.g. color scales.
+            Defaults to False.
+
+    Returns:
+        out_series (pd.Series): Mapped series.
+    """
+
+    # Sets incoming min and max to series values if none are given.
+    if in_min is None:
+        in_min = in_series.min()
+
+    if in_max is None:
+        in_max = in_series.max()
+
+    # Map series to new range.
+    amplitude = in_max - in_min
+    out_series = (in_series - in_min)/amplitude
+    # Apply clipping to prevent out-of-range values.
+    out_series.clip(lower=out_min, upper=out_max, inplace=True)
+
+    # If inversion if requested, do that.
+    if invert:
+        out_series = 1 - out_series
+
+    return out_series
