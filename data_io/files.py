f36a0b93f1171e6f4df3ac2b5a5f64091c269a77
Nick Weppenaar <nick.weppenaar@maersk.com>
2021-04-12 15:05:04 +0200

    Consolidated the load function.

--- data_io/files.py
+++ data_io/files.py
@@ -3,8 +3,6 @@ import numpy as np
 import os
 import pandas as pd
 
-from vc.data_io import files
-
 # TODO Change to include the aggregated data as another mode.
 def braz_cities_temp(folder_path: str, mode: str='dict'):
 
@@ -40,22 +38,22 @@ def braz_cities_temp(folder_path: str, mode: str='dict'):
 
         return file_dict
 
+    elif mode == 'agg':
 
-def braz_cities_temp_all(folder_path: str) -> pd.DataFrame:
-    # Get file name list from the folder.
-    file_name_list = os.listdir(folder_path)
+        df = pd.DataFrame()
 
-    df = pd.DataFrame()
+        for file_name in file_name_list:
+            # Load data into Pandas DataFrame with first row as column names and first column as index names.
+            city_df = load_file(os.path.join(folder_path, file_name))
+            city_df.columns = [idx+1 for idx, _ in enumerate(city_df.columns)]
+            stacked_df = city_df.stack()
+            stacked_df.index = [datetime.date(i[0], i[1], 1) for i in stacked_df.index]
+            df = pd.concat([df, pd.DataFrame({file_name: stacked_df})], axis=1)
 
-    for file_name in file_name_list:
-        # Load data into Pandas DataFrame with first row as column names and first column as index names.
-        city_df = files.braz_cities_temp(os.path.join(folder_path, file_name))
-        city_df.columns = [idx+1 for idx, _ in enumerate(city_df.columns)]
-        stacked_df = city_df.stack()
-        stacked_df.index = [datetime.date(i[0], i[1], 1) for i in stacked_df.index]
-        df = pd.concat([df, pd.DataFrame({file_name: stacked_df})], axis=1)
+        df.sort_index(inplace=True)
+        df.columns = [fname_to_city_name(file_name) for file_name in df.columns]
 
-    df.sort_index(inplace=True)
-    df.columns = [file_name[8:-4].replace('_', ' ').title() for file_name in df.columns]
+        return df
 
-    return df
+    else:
+        raise KeyError(f"Mode {mode} not allowed.")
