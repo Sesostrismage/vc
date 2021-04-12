81e2c818c55c397ac9f695744951465f5a5bc270
Nick Weppenaar <weppenaar@gmail.com>
2021-04-12 18:22:42 +0200

    Added selected cities list widget.

--- visuals/streamlit_tools.py
+++ visuals/streamlit_tools.py
@@ -18,3 +18,16 @@ def file_name_from_folder(folder_path: str):
     city_name = file_name[8:-4].replace('_', ' ').title()
 
     return file_name, city_name, file_name_list
+
+def multiselect_cities(data_dict: dict) -> list:
+    selected_cities_list = st.sidebar.multiselect(
+        'Select cities to view',
+        options=list(data_dict.keys()),
+        default=list(data_dict.keys())
+    )
+
+    if len(selected_cities_list) == 0:
+        st.error('No cities are selected.')
+        st.stop()
+
+    return selected_cities_list
