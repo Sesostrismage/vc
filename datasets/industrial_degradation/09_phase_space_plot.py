eef717d23ec4add64957da5139c351cb7fc22264
Nick Weppenaar <weppenaar@gmail.com>
2021-04-12 04:27:56 +0200

    Committing  until I'm sure I don't need it.

--- datasets/industrial_degradation/09_phase_space_plot.py
+++ datasets/industrial_degradation/09_phase_space_plot.py
@@ -9,46 +9,27 @@ import vc.visuals.plotly_tools.layout as pt_layout
 import vc.visuals.streamlit_tools as stt
 
 stt.settings()
-data_folder = r"C:/Data/mining"
+data_folder = r"C:/Data/spotify"
 df = pd.read_csv(
-    os.path.join(data_folder, 'MiningProcess_Flotation_Plant_Database.csv'),
-    parse_dates=['date'],
-    decimal=',',
+    os.path.join(data_folder, 'data.csv'),
     index_col=0,
-    header=0
+    header=0,
+    low_memory=False
 )
-df.index = pd.to_datetime(df.index)
+
+df.drop('release_date', axis=1, inplace=True)
 st.dataframe(df.head())
+st.write(df.dtypes)
 
 st.info(f"Total data size: {len(df)} rows.")
 
-date_start = st.sidebar.date_input(
-    'Data start date',
-    min_value=df.index[0].date(),
-    max_value=df.index[-1].date(),
-    value=df.index[0].date()
-)
-
-date_end = st.sidebar.date_input(
-    'Data start date',
-    min_value=df.index[0].date(),
-    max_value=df.index[-1].date(),
-    value=df.index[-1].date()
-)
-
-plot_df = df[(df.index >= datetime.datetime.combine(date_start, datetime.datetime.min.time())) & (df.index <= datetime.datetime.combine(date_end, datetime.datetime.min.time()))]
-
-options = list(df.columns)
+options = list(df.select_dtypes(exclude=['object']).columns)
 
 x = st.sidebar.selectbox(
     'Choose X variable',
-    options=['Datetime'] + options,
+    options=options,
     index=0
 )
-if x == 'Datetime':
-    x = plot_df.index
-else:
-    x = plot_df[x]
 
 y = st.sidebar.selectbox(
     'Choose y variable',
@@ -66,10 +47,10 @@ fig = go.Figure()
 
 fig.add_trace(
     go.Scattergl(
-        x=x,
-        y=plot_df[y],
+        x=df[x],
+        y=df[y],
         mode='markers',
-        marker={'color': plot_df[color]}
+        marker={'color': df[color]}
     )
 )
 
---
+++ definitions.py
@@ -0,0 +1,1 @@
+#TODO Create root_dir.
