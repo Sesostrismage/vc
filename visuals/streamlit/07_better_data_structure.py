b7fcb99669ec99f56b7d8fbd37468303dde522c4
Nick Weppenaar <weppenaar@gmail.com>
2021-04-17 14:58:30 +0200

    Better plotting.

--- visuals/streamlit/07_better_data_structure.py
+++ visuals/streamlit/07_better_data_structure.py
@@ -44,7 +44,6 @@ for file_name in file_name_list:
     df = pd.concat([df, pd.DataFrame({city_name: stacked_df})], axis=1)
 
 df.sort_index(inplace=True)
-city_list = list(df.columns)
 
 # Get fixed colormap.
 cmap = map_color_sequence(df.columns)
@@ -55,41 +54,69 @@ cmap = map_color_sequence(df.columns)
 ####################################################################
 
 # Multi-select which cities to plot.
-selected_cities_list = st.sidebar.multiselect(
+city_idx = st.sidebar.multiselect(
     'Select cities to view',
-    options=city_list,
-    default=city_list
+    options=list(df.columns),
+    default=[df.columns[0]]
 )
 # Check if any cities have been selected and warn the user if not.
-if len(selected_cities_list) == 0:
+if len(city_idx) == 0:
     st.error('No cities are selected.')
     st.stop()
 
-selected_df = df[[city for city in selected_cities_list]].dropna(how='all', axis=0)
+city_slice_df = df[city_idx]
+na_idx = city_slice_df.isnull().all(axis=1)
 
-date_start = st.sidebar.select_slider(
-    'Start date',
-    options=list(selected_df.index)
+# Choose whether or not to only show a single month per year.
+month_bool = st.sidebar.checkbox(
+    'Filter by month?',
+    value=False
 )
+if month_bool:
+    month = st.sidebar.slider(
+        'Choose month',
+        min_value=1,
+        max_value=12,
+        step=1
+    )
 
-date_end = st.sidebar.select_slider(
-    'End date',
-    options=list(selected_df.loc[date_start:].index),
-    value=selected_df.index[-1]
-)
+    year_list = sorted(set([dt.year for dt in df.loc[~na_idx].index]))
 
-sliced_df = selected_df.loc[date_start:date_end]
+    year_start = st.sidebar.slider(
+        'Start year',
+        min_value=year_list[0],
+        max_value=year_list[-1]
+    )
 
-# Choose reference data type.
-ref_type = st.sidebar.selectbox(
-    'Choose type of reference data',
-    options=['City', 'Month']
-)
+    year_end = st.sidebar.slider(
+        'End year',
+        min_value=year_start,
+        max_value=year_list[-1],
+        value=year_list[-1]
+    )
 
-if ref_type == 'City':
-    mean_series = df.loc[date_start:date_end].mean(axis=1)
-    min_series = df.loc[date_start:date_end].min(axis=1)
-    max_series = df.loc[date_start:date_end].max(axis=1)
+    dt_idx = [True if ((year_start <= dt.year <= year_end) and (dt.month == month)) else False for dt in df.index]
+    dt_city_slice_df = df.loc[dt_idx & ~na_idx, city_idx]
+    st.dataframe(dt_city_slice_df)
+
+else:
+    date_start = st.sidebar.select_slider(
+        'Start date',
+        options=list(city_slice_df.index)
+    )
+
+    date_end = st.sidebar.select_slider(
+        'End date',
+        options=list(city_slice_df.loc[date_start:].index),
+        value=list(city_slice_df.index)[-1]
+    )
+
+    dt_idx = (date_start <= df.index) & (df.index <= date_end)
+    dt_city_slice_df = df.loc[dt_idx & ~na_idx, city_idx]
+
+min_series = df.loc[dt_idx].min(axis=1)
+max_series = df.loc[dt_idx].max(axis=1)
+mean_series = df.loc[dt_idx].mean(axis=1)
 
 
 ####################################################################
@@ -99,52 +126,51 @@ if ref_type == 'City':
 # Create figure.
 fig = go.Figure()
 
-# Case when reference shapes are chosen.
-if ref_type == 'City':
-    # Handle missing data.
-    # Find indices where the series doesn't have null values.
-    valid_idx = min_series.notnull()
-    # Use those indices to put together x and y value lists.
-    x_part = list(min_series[valid_idx].index)
-    x = x_part + list(reversed(x_part))
+# Handle missing data.
+# Find indices where the series doesn't have null values.
+valid_idx = min_series.notnull()
+# Use those indices to put together x and y value lists.
+x_part = list(min_series[valid_idx].index)
+x = x_part + list(reversed(x_part))
 
-    y_mean_part = [val for idx, val in enumerate(mean_series) if valid_idx[idx]]
-    y_part = [val for idx, val in enumerate(min_series) if valid_idx[idx]]
-    y_min = y_part + list(reversed(y_mean_part))
-    y_part = [val for idx, val in enumerate(max_series) if valid_idx[idx]]
-    y_max = y_part + list(reversed(y_mean_part))
+y_mean_part = [val for idx, val in enumerate(mean_series) if valid_idx[idx]]
 
-    # Plot the min and max areas as filled polygons.
-    fig.add_trace(
-        go.Scatter(
-            x=x,
-            y=y_min,
-            fill='toself',
-            mode='none',
-            marker={'color': get_color('temperature', 'min')},
-            showlegend=False,
-            hoverinfo='none'
-        )
+y_part = [val for idx, val in enumerate(min_series) if valid_idx[idx]]
+y_min = y_part + list(reversed(y_mean_part))
+y_part = [val for idx, val in enumerate(max_series) if valid_idx[idx]]
+y_max = y_part + list(reversed(y_mean_part))
+
+# Plot the min and max areas as filled polygons.
+fig.add_trace(
+    go.Scatter(
+        x=x,
+        y=y_min,
+        fill='toself',
+        mode='none',
+        marker={'color': get_color('temperature', 'min')},
+        showlegend=False,
+        hoverinfo='none'
     )
-    fig.add_trace(
-        go.Scatter(
-            x=x,
-            y=y_max,
-            fill='toself',
-            mode='none',
-            marker={'color': get_color('temperature', 'max')},
-            showlegend=False,
-            hoverinfo='none'
-        )
+)
+fig.add_trace(
+    go.Scatter(
+        x=x,
+        y=y_max,
+        fill='toself',
+        mode='none',
+        marker={'color': get_color('temperature', 'max')},
+        showlegend=False,
+        hoverinfo='none'
     )
+)
 
 # Plot all selected cities.
-for city_name in sliced_df.columns:
-        text_list = ['So empty...' for idx, row in sliced_df.iterrows()]
+for city_name in dt_city_slice_df.columns:
+        text_list = ['So empty...' for idx, row in dt_city_slice_df.iterrows()]
 
         fig.add_trace(go.Scattergl(
-            x=sliced_df.index,
-            y=sliced_df[city_name],
+            x=dt_city_slice_df.index,
+            y=dt_city_slice_df[city_name],
             hoverinfo='text',
             hovertext=text_list,
             line={'color': cmap[city_name]},
