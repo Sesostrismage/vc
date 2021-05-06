933aa1ab4074fd932810f65899acc4fcf3a1fc48
Nick Weppenaar <weppenaar@gmail.com>
2021-05-06 17:22:36 +0200

    Added comments to more files.

--- visuals/colors.py
+++ visuals/colors.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 
+# Dict for all standard colours to be used in this module.
 cdict = {
     'temperature': {
         'value': 'black',
@@ -15,9 +16,11 @@ cdict = {
     }
 }
 
+# Function to get any given colour.
 def get_color(cat1: str, cat2: str) -> str:
     return cdict[cat1][cat2]
 
+# Map a list of items to the Alphabet colour sequence to get a consistent sequence of colours.
 def map_color_sequence(input_list: list) -> dict:
     cmap = {}
     sorted_list = sorted(input_list)
