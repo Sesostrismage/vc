7308af8070209363e1d77d3d5bffb6f5a8e48dbf
Nick Weppenaar <weppenaar@gmail.com>
2021-05-06 17:24:32 +0200

    Remved superfluous functions.

--- visuals/plotly_tools/trace.py
+++ visuals/plotly_tools/trace.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-from vc.visuals.colors import get_color
 import vc.visuals.plotly_tools.hovertext as pt_hover
 
 def braz_cities_temp(
@@ -23,57 +22,3 @@ def braz_cities_temp(
     ))
 
     return fig
-
-
-def braz_cities_temp_v1(series: pd.Series, city_name: list, trace_name: str) -> go.Scatter:
-    text_list = pt_hover.braz_cities_temp(series, city_name, trace_name)
-
-    trace = go.Scatter(
-        x=series.index,
-        y=series,
-        hoverinfo='text',
-        hovertext=text_list,
-        name=str(trace_name),
-        mode='lines+markers'
-    )
-
-    return trace
-
-
-def braz_cities_temp_v2(
-    series: pd.Series,
-    city_name: list,
-    trace_name: str,
-    cat1: str,
-    cat2: str,
-    background: bool=False,
-    ref_df: pd.DataFrame=None
-) -> go.Scatter:
-    text_list = pt_hover.braz_cities_temp(
-        series,
-        city_name,
-        trace_name,
-        ref_df=ref_df
-    )
-
-
-    color = get_color(cat1, cat2)
-
-    if background:
-        opacity = 0.25
-    else:
-        opacity = 1
-
-    trace = go.Scatter(
-        x=series.index,
-        y=series,
-        hoverinfo='text',
-        hovertext=text_list,
-        name=str(trace_name),
-        mode='lines+markers',
-        line={'color': color},
-        marker={'color': color},
-        opacity=opacity
-    )
-
-    return trace
