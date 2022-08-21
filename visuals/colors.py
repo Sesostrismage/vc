b193b3b1c308e61b594755278b61dabcd650c33e
Sesostrismage <Sesostrismage@users.noreply.github.com>
2022-08-21 16:06:51 +0200

    Moved cdict inside the function so it can only be accessed that way. (#28)

--- visuals/colors.py
+++ visuals/colors.py
@@ -1,19 +1,23 @@
 import plotly.express as px
 
-# Dict for all standard colours to be used in this module.
-cdict = {
-    "seasons": {"Summer": "red", "Winter": "blue"},
-    "temperature": {"value": "black", "mean": "#999999", "min": "blue", "max": "red"},
-    "labeling": {
-        "Add": "#00ff00",
-        "Remove": "red",
-        "default": "black",
-        "labeled": "magenta",
-    },
-}
-
 # Function to get any given colour.
 def get_color(cat1: str, cat2: str) -> str:
+    # Dict for all standard colours to be used in this module.
+    cdict = {
+        "seasons": {"Summer": "red", "Winter": "blue"},
+        "temperature": {
+            "value": "black",
+            "mean": "#999999",
+            "min": "blue",
+            "max": "red",
+        },
+        "labeling": {
+            "Add": "#00ff00",
+            "Remove": "red",
+            "default": "black",
+            "labeled": "magenta",
+        },
+    }
     return cdict[cat1][cat2]
 
 
