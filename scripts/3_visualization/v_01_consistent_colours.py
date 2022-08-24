fcc9094d3536a085d488f32b2a516dee4b408cb1
Nick Weppenaar <weppenaar@gmail.com>
2022-08-24 10:15:35 +0200

    Updated consistent_colors.

--- scripts/3_visualization/v_01_consistent_colours.py
+++ scripts/3_visualization/v_01_consistent_colours.py
@@ -1,168 +1,54 @@
 from pathlib import Path
 
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
-from vc.definitions import ROOT_DIR
+import vc.visuals.plotly_tools.figure as pt_figure
+import vc.visuals.plotly_tools.trace as pt_trace
+import vc.visuals.streamlit_tools as stt
+from vc.datasets.temp_brazil_cities.cities_data import CitiesTempData
+from vc.visuals.colors import map_color_sequence
 
 ####################################################################
 # Setup and data loading.
 ####################################################################
 
 # Standard Streamlit settings.
-st.set_page_config(layout="wide")
+stt.settings()
 # Title becomes the file name for easy reference to the presentation.
 st.title(Path(__file__).name)
-# Folder path with root of vc directory automatically detected.
-folder_path = ROOT_DIR / "datasets" / "temp_brazil_cities" / "raw_data"
-# File name list from reading the folder contents.
-file_name_list = list(folder_path.iterdir())
-# Empty dict to receive data.
-city_dict = {}
-
-# Loop through all file names and load the data.
-for file_name in file_name_list:
-    # Generate city name from file name.
-    city_name = file_name.name[8:-4].replace("_", " ").title()
-    # Load data into Pandas DataFrame with first row as column names and first column as index names.
-    df = pd.read_csv(folder_path / file_name, header=0, index_col=0)
-    # Remove pre-generated average columns.
-    df_crop = df.drop(["D-J-F", "M-A-M", "J-J-A", "S-O-N", "metANN"], axis=1)
-    # Set erroneous values to NaN so they don't disturb the results.
-    df_crop[df_crop > 100] = np.nan
-    # Insert dataframe into file dict.
-    city_dict[city_name] = df_crop
-
-# Create a dictionary to hold consistent colours for each city.
-cdict = {}
-# Ensure that the cities are always sorted in the same order.
-sorted_list = sorted(city_dict.keys())
-# Assign colours to each city.
-for idx, item in enumerate(sorted_list):
-    cdict[item] = px.colors.qualitative.Alphabet[idx]
+# Object with city temp data.
+city_data = CitiesTempData()
 
 
 ####################################################################
 # User input and calculations.
 ####################################################################
 
-# Multi-select files to load, with all files chosen by default from the dict.
-selected_cities_list = st.sidebar.multiselect(
-    "Select cities to view",
-    options=sorted(list(city_dict.keys())),
-    default=sorted(list(city_dict.keys())),
-)
+# Multi-select which cities to plot.
+stt.multiselect_cities(city_data)
 
-# If no files are chosen, show a warning and stop the program.
-if len(selected_cities_list) == 0:
-    st.error("No cities are selected.")
-    st.stop()
+plot_df, stat_dict, month = stt.braz_cities_choose_data(city_data)
 
-# Find earliest and latest years that have data.
-# Start with a year that is in all cities' data.
-min_year = 2010
-max_year = 2010
-# Loop through all cities and find the earliest and latest year with data.
-for city in selected_cities_list:
-    if city in city_dict:
-        min_year = min(min_year, city_dict[city].index[0])
-        max_year = max(max_year, city_dict[city].index[-1])
-
-# Get all available years from the file and make it into a list.
-year_list = range(min_year, max_year + 1)
-# Selectbox to choose the year.
-year = st.sidebar.selectbox(
-    "Choose year to view", options=year_list, index=len(year_list) - 1
-)
-# Choose whether or not to having consistent colours assigned to the cities.
-consistent_color_bool = st.sidebar.checkbox(
-    "Consistent colormap?",
-)
 # Choose whether or not to show the mean value line.
 show_mean_bool = st.sidebar.checkbox("Show mean value?")
-# If yes, build the mean dataframe.
+
+# # If yes, build the mean series.
 if show_mean_bool:
-    mean_df = pd.DataFrame()
-    # Loop through all cities.
-    for city_name in city_dict:
-        if year in city_dict[city_name].index:
-            # Build mean df.
-            mean_df = pd.concat(
-                [mean_df, pd.DataFrame({city_name: city_dict[city_name].loc[year]})],
-                axis=1,
-            )
-    # Calculate the mean series.
-    mean_series = mean_df.mean(axis=1)
+    # Create statistical series.
+    mean_series = plot_df.mean(axis=1)
 
 
 ####################################################################
 # Plotting.
 ####################################################################
 
-# Create a Plotly figure.
-fig = go.Figure()
-# Layout.
-fig.update_xaxes(title="Datetime")
-fig.update_yaxes(title="Temperature [deg C]")
-fig.update_layout(
-    title=f"Temperature for brazilian cities in {year}",
-    hovermode="x",
-    height=800,
-    width=1400,
-    plot_bgcolor="#ffffff",
-)
-# Dict to set better axis properties.
-axis_dict = {
-    # Move ticks outside the plot.
-    "ticks": "outside",
-    # Show plot borders with these four settings.
-    "showline": True,
-    "linewidth": 2,
-    "linecolor": "black",
-    "mirror": True,
-    # Remove gridlines in the plot.
-    "showgrid": False,
-}
-# Apply to both axes.
-fig.update_xaxes(axis_dict)
-fig.update_yaxes(axis_dict)
-
-# Plot each chosen city if it has data.
-for city_name in (city for city in city_dict if city in selected_cities_list):
-    if year in city_dict[city_name].index:
-        if consistent_color_bool:
-            # Plot data from selected year if present.
-            # use the assigned colour for each city.
-            fig.add_trace(
-                go.Scattergl(
-                    x=city_dict[city_name].columns,
-                    y=city_dict[city_name].loc[year],
-                    line={"color": cdict[city_name]},
-                    name=city_name,
-                )
-            )
-        else:
-            # Else plot without consistent colour assignment.
-            fig.add_trace(
-                go.Scattergl(
-                    x=city_dict[city_name].columns,
-                    y=city_dict[city_name].loc[year],
-                    name=city_name,
-                )
-            )
-
-# Plot mean line if chosen.
+# Create figure.
+fig = pt_figure.braz_cities_temp_per_year(month=month)
+# Get a consistent colormap.
+cmap = map_color_sequence(city_data.get_cities(selection_only=False))
+# Plot all selected cities with consistent colors.
+fig = pt_trace.braz_cities_temp(fig, city_data, month, cmap=cmap)
+# Plot statistical series if chosen.
 if show_mean_bool:
-    fig.add_trace(
-        go.Scattergl(
-            x=mean_series.index,
-            y=mean_series,
-            name="All-city mean",
-            line={"color": "grey"},
-        )
-    )
-
+    pt_trace.mean_series(fig, mean_series)
 # Show the figure in the Streamlit app.
 st.plotly_chart(fig)
